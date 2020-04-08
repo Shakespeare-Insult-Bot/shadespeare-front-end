105dbd71f74bf2d29ea3d0e4fe28e68c7f3ac182
Scott Harlan <seharlan@gmail.com>
2020-04-07 17:47:02 -0700

    font end css

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,8 @@ import request from 'superagent'
 
 export default class Home extends Component {
   state = { 
-    response: 'Whatever you do, do not press this button'
+    response: '',
+    warningBool: true 
   }
 
   handleClick = async() => {
@@ -15,7 +16,8 @@ export default class Home extends Component {
       const quoteData = await request.post('https://shadespeare-staging.herokuapp.com/api/v1/tweets')
       // const quoteData = await request.post('http://localhost:7890/api/v1/tweets')
       this.setState({
-        response: quoteData.body.tweetText
+        response: quoteData.body.tweetText,
+        warningBool: false
       })
     } catch(err){
       console.log(err);
@@ -24,15 +26,22 @@ export default class Home extends Component {
     
   render() {
     return (
-      <div>
-      <header>  THIS IS HEADER    </header>
+      <div className="home-page">
 
-      <Link to='/about-us'>About Us</Link>
-      <h2>
-        {this.state.response}
-      </h2>
-      <button onClick={this.handleClick}>DONT PRESS</button>
+      <div className="shadeField">
+        <img className="shadespeare" src="shadespeare.png" alt="shadespeare" />
+        <h2 className="response">{this.state.response}</h2>
+      </div>
+      
+
+      <div className="buttonField">
+      <button><img className="scrollImage" src="scroll&pen.png" alt="the button" onClick={this.handleClick} /></button>
+        <p>
+          <em>{this.state.warningBool ? 'Whatev\'r thee doth click not yond scroll!' : '...I hath tried to warneth thee'}</em>
+        </p>
+      </div>
         
+      <Link to='/about-us'>About Us</Link>
       </div>
     )
   }
