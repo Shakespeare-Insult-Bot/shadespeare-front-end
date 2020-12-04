c5dc7eede9e63554ecf2089c8a92f6961cf4fd37
Scott <seharlan@gmail.com>
2020-12-03 16:17:18 -0800

    removed failing photos

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -11,7 +11,7 @@ export default class AboutUs extends Component {
                 <h1 className='title'>Shadespeare, Bard of Insults</h1>
 
                 <div className='card-div'>
-                    <img src="https://i.imgur.com/hAIRYe1.png" alt="card background"  className="card-img"></img>
+                    {/* <img src="https://i.imgur.com/hAIRYe1.png" alt="card background" className="card-img"></img> */}
                     <div>
                         <h2 className='our-names'>Fiona Ochs</h2>
                         <p>"Thine be a hamster and lack gall." <em>-Shadespeare</em></p>
@@ -23,43 +23,43 @@ export default class AboutUs extends Component {
                 </div>
 
                 <div className='card-div'>
-                    <img src="https://i.imgur.com/WlLReq6.png" alt="card background" className="card-img"></img>
+                    {/* <img src="https://i.imgur.com/WlLReq6.png" alt="card background" className="card-img"></img> */}
                     <div className="scott">
                         <h2 className='our-names'>Scott Harlan</h2>
                         <p>"Well, every one can master a piece of trash like you." <em>-Shadespeare</em></p>
                         <div className='our-links'>
                             <a href="https://www.linkedin.com/in/scottharlan-pnw/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
                             <a href="https://github.com/SeHarlan" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
-                            </div>
                         </div>
                     </div>
-                    
-                    <div className='card-div'>
-                        <img src="https://i.imgur.com/KmTHuQH.png" alt="card background" className="card-img"></img>
-                        <div>
-                            <h2 className='our-names'>Cody Brown</h2>
-                            <p>"You Bastard" <em>-Shadespeare</em></p>
-                            <div className='our-links'>
-                                <a href="https://www.linkedin.com/in/codylylebrown/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
-                                <a href="https://github.com/cody2934" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='card-div'>
-                        <img src="https://i.imgur.com/R0wRlu4.png" alt="card background" className="card-img"></img>
-                        <div>
-                            <h2 className='our-names'>James Eserjose</h2>
-                            <p>"As much pleasure, just so much joy, when thee leave the room." <em>-Shadespeare</em></p>
-                            <div className='our-links'>
-                                <a href="https://www.linkedin.com/in/jamesreserjose/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
-                                <a href="https://github.com/ezjim" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
-                            </div>
-                        </div>
-                    </div>
-                    <Link className="home" to='/'>Back to home</Link>
-                    <Link className="about-Project" to='/about-project'>About Shadespeare</Link>
                 </div>
-                )
-            }
-        }
+
+                <div className='card-div'>
+                    {/* <img src="https://i.imgur.com/KmTHuQH.png" alt="card background" className="card-img"></img> */}
+                    <div>
+                        <h2 className='our-names'>Cody Brown</h2>
+                        <p>"You Bastard" <em>-Shadespeare</em></p>
+                        <div className='our-links'>
+                            <a href="https://www.linkedin.com/in/codylylebrown/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+                            <a href="https://github.com/cody2934" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='card-div'>
+                    {/* <img src="https://i.imgur.com/R0wRlu4.png" alt="card background" className="card-img"></img> */}
+                    <div>
+                        <h2 className='our-names'>James Eserjose</h2>
+                        <p>"As much pleasure, just so much joy, when thee leave the room." <em>-Shadespeare</em></p>
+                        <div className='our-links'>
+                            <a href="https://www.linkedin.com/in/jamesreserjose/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+                            <a href="https://github.com/ezjim" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+                        </div>
+                    </div>
+                </div>
+                <Link className="home" to='/'>Back to home</Link>
+                <Link className="about-Project" to='/about-project'>About Shadespeare</Link>
+            </div>
+        )
+    }
+}
